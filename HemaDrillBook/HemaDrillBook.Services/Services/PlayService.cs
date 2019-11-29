47f21a0411b90b7d523f7a441768ee86c45254f3
Jonathan Allen <grauenwolf@gmail.com>
2019-11-29 12:32:01 -0800

    Add Video Button #52

--- HemaDrillBook/HemaDrillBook.Services/Services/PlayService.cs
+++ HemaDrillBook/HemaDrillBook.Services/Services/PlayService.cs
@@ -1,5 +1,8 @@
 ﻿using HemaDrillBook.Models;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Tortuga.Chain;
 
 namespace HemaDrillBook.Services
@@ -42,6 +45,48 @@ namespace HemaDrillBook.Services
                 dataSource.Commit();
             }
             return play.PlayKey.Value;
+        }
+
+        public async Task<int> AddVideoAsync(VideoInput video, IUser currentUser)
+        {
+            await CheckPermissionSectionAsync(video.SectionKey, currentUser);
+
+            if (video == null)
+                throw new ArgumentNullException(nameof(video), $"{nameof(video)} is null.");
+            if (video.Url == null)
+                throw new ArgumentNullException(nameof(video), $"{nameof(video)}.{video.Url} is null.");
+
+            //https://www.youtube.com/watch?v=6ISOK-XtvYs
+
+            if (video.Url.Contains("www.youtube.com") || video.Url.Contains("youtu.be"))
+            {
+                var uri = new Uri(video.Url);
+                var query = HttpUtility.ParseQueryString(uri.Query);
+
+                if (query.AllKeys.Contains("v"))
+                {
+                    video.VideoServiceVideoId = query["v"];
+                }
+                else
+                {
+                    video.VideoServiceVideoId = uri.Segments.Last();
+                }
+
+                video.VideoServiceKey = 1;
+            }
+            else
+            {
+                video.VideoServiceKey = 0;
+            }
+
+            if (string.IsNullOrWhiteSpace(video.Description))
+                video.Description = null;
+            if (string.IsNullOrWhiteSpace(video.Url))
+                video.Url = null;
+            if (string.IsNullOrWhiteSpace(video.Author))
+                video.Author = null;
+
+            return await DataSource(currentUser).Insert("Interpretations.Video", video).ToInt32().ExecuteAsync();
         }
     }
 }
--- HemaDrillBook/HemaDrillBook/Pages/Section.razor.cs
+++ HemaDrillBook/HemaDrillBook/Pages/Section.razor.cs
@@ -1,6 +1,7 @@
 ﻿using HemaDrillBook.Models;
 using HemaDrillBook.Services;
 using Microsoft.AspNetCore.Components;
+using System;
 using System.Threading.Tasks;
 
 namespace HemaDrillBook.Pages
@@ -9,6 +10,7 @@ namespace HemaDrillBook.Pages
     {
 #nullable disable
         [Inject] BookService BookService { get; set; }
+        [Inject] PlayService PlayService { get; set; }
         [Inject] protected NavigationManager Navigation { get; set; }
 #nullable restore
 
@@ -38,6 +40,45 @@ namespace HemaDrillBook.Pages
         {
             get => Get<string?>();
             set => Set(value, false);
+        }
+
+        protected VideoInput VideoInput { get; set; } = new VideoInput();
+
+        protected async void SubmitVideo()
+        {
+            //These should never happen.
+            if (Model == null)
+            {
+                VideoInput.ErrorDisplay = "Section not loaded.";
+                return;
+            }
+            if (User == null)
+            {
+                VideoInput.ErrorDisplay = "User not logged in.";
+                return;
+            }
+
+            VideoInput.SectionKey = Model.SectionKey;
+
+            try
+            {
+                //Save goes here
+                await PlayService.AddVideoAsync(VideoInput, User);
+
+                //Reset the page
+                VideoInput = new VideoInput();
+                StateHasChanged();
+                Navigation.NavigateTo(Model.SectionUrlFragment + "/t/videos");
+                Model = null; //need to refresh data
+            }
+            catch (Exception ex) when (ex.Message.Contains("UX_Video_NoDup"))
+            {
+                VideoInput.ErrorDisplay = "Unable to save: This video was already attached to this section." + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                VideoInput.ErrorDisplay = "Unable to save: " + ex.Message;
+            }
         }
 
         protected override async Task ParametersSetAsync()
