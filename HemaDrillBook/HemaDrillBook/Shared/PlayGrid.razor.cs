0f44c67e6970b258fb5389ee6044a7608d35b551
Jonathan Louis Allen <grauenwolf@gmail.com>
2023-05-11 16:28:54 -0700

    Trying new colors for play grid

--- HemaDrillBook/HemaDrillBook/Shared/PlayGrid.razor.cs
+++ HemaDrillBook/HemaDrillBook/Shared/PlayGrid.razor.cs
@@ -5,10 +5,11 @@ namespace HemaDrillBook.Shared
 {
     partial class PlayGrid
     {
+        protected const string agentStyle = "background-color:#C3DDB8";
+
+        protected const string patientStyle = "background-color:#FFF5CC";
+
         [Parameter]
         public PlayDetail? Play { get; set; }
-
-        protected const string agentStyle = "background-color:mistyrose";
-        protected const string patientStyle = "background-color:khaki";
     }
 }
